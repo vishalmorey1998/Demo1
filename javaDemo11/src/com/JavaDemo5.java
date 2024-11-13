ca99350cb2e3ee5fb42c3b03db00ba5588e92175
HP <vishalmorey1998@gmail.com>
2024-11-13 12:29:23 +0530

    by pooja

---
+++ javaDemo11/src/com/JavaDemo5.java
@@ -0,0 +1,34 @@
+package com;
+
+public class JavaDemo5 {
+	public void m1()
+	{
+        System.out.println("This is a M1 method .....");
+
+		
+	}
+	public void m2()
+	{
+        System.out.println("This is a M2 method .....");
+
+		
+	}
+	
+	public void m3()
+	{
+        System.out.println("This is a M3 method .....");
+
+		
+	}
+	public void m4()
+	{
+        System.out.println("This is a M4 method .....");
+
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("This is a Github process");
+
+	}
+
+}
